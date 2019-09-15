1be014fdb61911183e7bb3e67e5c9796f4f2a769
Xiomara <xiomara.masmela@gmail.com>
2019-09-15 15:39:49 +1000

    updates

---
+++ build/precache-manifest.d9999b358582cd1274d632dd68694c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d349093068be8c2be6597145a3f71d",
+    "url": "xiomara-masmela.github.io/index.html"
+  },
+  {
+    "revision": "108e726a9203b9650ae1",
+    "url": "xiomara-masmela.github.io/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "704f518e4a2ae3b0e930",
+    "url": "xiomara-masmela.github.io/static/js/2.428394aa.chunk.js"
+  },
+  {
+    "revision": "108e726a9203b9650ae1",
+    "url": "xiomara-masmela.github.io/static/js/main.c209730f.chunk.js"
+  },
+  {
+    "revision": "73a7e47d3b65322ef6e4",
+    "url": "xiomara-masmela.github.io/static/js/runtime~main.b93b3c43.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d59819c59c6e8744fdaad73431a2c6a.js"
+  "xiomara-masmela.github.io/precache-manifest.d9999b358582cd1274d632dd68694c4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("xiomara-masmela.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

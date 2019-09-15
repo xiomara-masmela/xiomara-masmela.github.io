6586f7a948a131d2014e52fe698b20ab1c09e643
Xiomara <xiomara.masmela@gmail.com>
2019-09-15 12:18:43 +1000

    Update node gh-pages

--- src/resumeData.js
+++ src/resumeData.js
@@ -2,6 +2,5 @@ let resumeData = {
     "imagebaseurl":"https://xiomara-masmela.github.io/",
     "name": "Xio",
     "role": "Frontend Developer",
-    "homepage": "https://xiomara-masmela.github.io/"
     }
   export default resumeData
